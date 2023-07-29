70d1a884ad62296097a66dd4efdf1d515683682e
UBC Student <liangtam@students.cs.ubc.ca>
2023-07-28 20:18:38 -0700

    Add files via upload
    1 file for all insert statements, 1 file for all table creation statements, and 2 files to view the tables created.

---
+++ Get_All_Tables2_SQLQuery4.sql
@@ -0,0 +1,29 @@
+SELECT TOP (1000)*
+FROM MusicManagement.dbo.Venue_Main
+
+SELECT TOP (1000)*
+FROM MusicManagement.dbo.Venue_LocationOne
+
+SELECT TOP (1000)*
+FROM MusicManagement.dbo.Venue_LocationTwo
+
+SELECT TOP (1000)*
+FROM MusicManagement.dbo.LiveEvent_IsHeldAt
+
+SELECT TOP (1000)*
+FROM MusicManagement.dbo.IsIn
+
+SELECT TOP (1000)*
+FROM MusicManagement.dbo.Includes
+
+SELECT TOP (1000)*
+FROM MusicManagement.dbo.PerformsAt
+
+SELECT TOP (1000)*
+FROM MusicManagement.dbo.Purchases
+
+SELECT TOP (1000)*
+FROM MusicManagement.dbo.Attends
+
+SELECT TOP (1000)*
+FROM MusicManagement.dbo.Releases
---
+++ Get_All_Tables_SQLQuery3.sql
@@ -0,0 +1,26 @@
+SELECT TOP (1000)*
+FROM MusicManagement.dbo.RecordLabel
+
+SELECT TOP (1000)*
+FROM MusicManagement.dbo.Artist_ContractedWith
+
+SELECT TOP (1000)*
+FROM MusicManagement.dbo.MusicListener
+
+SELECT TOP (1000)*
+FROM MusicManagement.dbo.Discography_Main
+
+SELECT TOP (1000)*
+FROM MusicManagement.dbo.Discography_Ranking
+
+SELECT TOP (1000)*
+FROM MusicManagement.dbo.Album
+
+SELECT TOP (1000)*
+FROM MusicManagement.dbo.Song
+
+SELECT TOP (1000)*
+FROM MusicManagement.dbo.Playlist_Created
+
+SELECT TOP (1000)*
+FROM MusicManagement.dbo.Merchandise_Sold
