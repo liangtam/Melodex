7d408c2c29397ee04f1a57a960f64395c90372e1
UBC Student <flora817@students.cs.ubc.ca>
2023-08-08 13:04:52 -0700

    Update AlbumForm.js

--- frontend/src/components/Album/AlbumForm.js
+++ frontend/src/components/Album/AlbumForm.js
@@ -9,6 +9,9 @@ const AlbumForm = () => {
             <h3>Add New Album</h3>
             <label>Album Name: <input type="text" placeholder="Album name"></input></label>
             <label>Artist Name: <input type="text" placeholder="Name(s)"></input></label>
+            <label>Genre: <input type="text" placeholder="Genre"></input></label>
+            <label>Release Date: <input type="text" placeholder="eg. 2000-00-00(?)"></input></label>
+            <label>Number of Likes: <input type="text" placeholder="1 or more"></input></label>
             <label>Number of Songs: <input type="text" placeholder="1 or more"></input></label>
             <label>Duration: <input type="text" placeholder="The length of album"></input></label>
             </div>
