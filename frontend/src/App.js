e0988845c289d0d75b6b8707a95d3d9606deb943
UBC Student <flora817@students.cs.ubc.ca>
2023-08-07 18:12:26 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Registration from './pages/Registration';
+import LogIn from './pages/LogIn';
 
 
 // This is where we will be storing our links to the webpages :)
@@ -13,8 +14,12 @@ function App() {
         <Navbar/>
         <div className='pages'>
           <Routes>
+    
+            <Route exact path="/loginPage" element={
+              <LogIn/>
+            }/>
 
-            <Route exact path="/" element={
+            <Route exact path="/SignupPage" element={
               <Registration/>
             }/>
             <Route exact path="/artists"/>
