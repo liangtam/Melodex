2ed420dfa2833e0a18e56bb23d420ce7e7bb98d7
Ravenclawer <ljw0708@students.cs.ubc.ca>
2023-08-07 18:36:34 -0700

    update artist login form

--- frontend/src/components/ArtistLogInForm.js
+++ frontend/src/components/ArtistLogInForm.js
@@ -2,14 +2,17 @@ import { useState, useEffect } from "react";
 import styles from "./ArtistLogInForm.module.css";
 
 const ArtistLogInForm = () => {
-
-    return (
-        <div className={styles.form}>
-        <label>Username: <input type="text" placeholder="Name"></input></label>
-        <label>Password: <input type="text" placeholder="Username"></input></label>
-        <button>Log In</button>
+  return (
+    <div className={styles.form}>
+      <label>
+        Username: <input type="text" placeholder="Name"></input>
+      </label>
+      <label>
+        Password: <input type="text" placeholder="Password"></input>
+      </label>
+      <button>Log In</button>
     </div>
-    )
-}
+  );
+};
 
 export default ArtistLogInForm;
