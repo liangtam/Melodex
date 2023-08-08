99732bb113202f37b2c168242e4651f554a6a502
UBC Student <flora817@students.cs.ubc.ca>
2023-08-07 18:09:03 -0700

    Create ListenerLogInForm.js

---
+++ frontend/src/components/ListenerLogInForm.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+import styles from "./ListenerLogInForm.module.css";
+
+const ListenerLogInForm = () => {
+
+    return (
+        <div className={styles.form}>
+        <label>Name: <input type="text" placeholder="Name"></input></label>
+        <label>Password: <input type="text" placeholder="Username"></input></label>
+        <button>Log In</button>
+    </div>
+    )
+}
+
+export default ListenerLogInForm;
