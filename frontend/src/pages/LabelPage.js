25b8b04341528230e22fb217af5d58540d0dfdca
Ravenclawer <ljw0708@students.cs.ubc.ca>
2023-08-08 14:01:03 -0700

    complete labelpage.js

---
+++ frontend/src/pages/LabelPage.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import styles from "./LabelPage.module.css";
+
+import SongForm from "../components/Labels/LabelForm";
+
+const LabelPage = () => {
+  const handleAddLabel = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className={styles.labelContainer}>
+      <div className={styles.leftBody}>
+        <div className={styles.centered}>
+          <div className={styles.title}>
+            <h4>Label</h4>
+          </div>
+        </div>
+      </div>
+      <div className={styles.rightBody}>
+        <div className={styles.centered}>
+          <div className={styles.addForm}>
+            <LabelForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// DO NOT FORGET THIS LINE! It is very important. If we don't export the page, we can't use it LOL
+export default LabelPage;
