eecaf85a05fd676da57c10bd8ec1151123f13451
Ravenclawer <ljw0708@students.cs.ubc.ca>
2023-08-08 15:20:15 -0700

    update display all artists

--- frontend/src/pages/DisplayAllArtists.js
+++ frontend/src/pages/DisplayAllArtists.js
@@ -1,32 +1,13 @@
 import { useState, useEffect } from "react";
-import styles from "./AlbumsPage.module.css";
+import styles from "./DisplayAllArtists.module.css";
 
-import AlbumForm from "../components/Album/AlbumForm";
-
-const AlbumsPage = () => {
-  const handleAddAlbum = (e) => {
+const DisplayAllArtists = () => {
+  const handleDisplay = (e) => {
     e.preventDefault();
   };
 
-  return (
-    <div className={styles.albumContainer}>
-      <div className={styles.leftBody}>
-        <div className={styles.centered}>
-          <div className={styles.title}>
-            <h4>Album</h4>
-          </div>
-        </div>
-      </div>
-      <div className={styles.rightBody}>
-        <div className={styles.centered}>
-          <div className={styles.addForm}>
-            <AlbumForm />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div>Hello World!</div>;
 };
 
 // DO NOT FORGET THIS LINE! It is very important. If we don't export the page, we can't use it LOL
-export default AlbumsPage;
+export default DisplayAllArtists;
