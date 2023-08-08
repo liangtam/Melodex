60d3cd5b776c290c175cd1aba064b60e19007502
Ravenclawer <ljw0708@students.cs.ubc.ca>
2023-08-07 18:49:17 -0700

    use email to log in

--- frontend/src/components/ListenerLogInForm.js
+++ frontend/src/components/ListenerLogInForm.js
@@ -5,7 +5,7 @@ const ListenerLogInForm = () => {
   return (
     <div className={styles.form}>
       <label>
-        Username: <input type="text" placeholder="Username"></input>
+        Email: <input type="text" placeholder="Email"></input>
       </label>
       <label>
         Password: <input type="text" placeholder="Password"></input>
