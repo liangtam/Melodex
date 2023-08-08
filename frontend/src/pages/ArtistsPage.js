199b348d90032451a03d25c310d8497605f24ae0
Ravenclawer <ljw0708@students.cs.ubc.ca>
2023-08-08 14:41:24 -0700

    update artist page

--- frontend/src/pages/ArtistsPage.js
+++ frontend/src/pages/ArtistsPage.js
@@ -4,35 +4,32 @@ import styles from "./ArtistsPage.module.css";
 import ArtistForm from "../components/Album/ArtistForm";
 
 const ArtistsPage = () => {
+  const handleAddArtist = (e) => {
+    e.preventDefault();
+  };
 
-
-
-    const handleAddArtist = (e) => {
-        e.preventDefault();
-
-    }
-
-    return (
-        <div className={styles.regContainer}>
-            <div className={styles.leftBody}>
-                <div className={styles.centered}>
-                    <div className={styles.title}>
-                        <h4>Artists</h4>
-                    </div>
-                </div>
-            </div>
-            <div className={styles.rightBody}>
-                <div className={styles.centered}>
-                    <div className={styles.addForm}>
-                        <ArtistForm/>
-                    </div>
-                </div>
-            </div>
-            
+  return (
+    <div className={styles.regContainer}>
+      <div className={styles.leftBody}>
+        <div className={styles.centered}>
+          <div className={styles.title}>
+            <h4>All the artists you like, right in our app.</h4>
+            <button>Display All Artists</button>
+          </div>
         </div>
-        
-    )
-}
+      </div>
+      <div className={styles.rightBody}>
+        <div className={styles.centered}>
+          <div className={styles.addForm}>
+            <p>Didn't see the artist of your choice?</p>
+            <p>Add them yourself!</p>
+            <ArtistForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 // DO NOT FORGET THIS LINE! It is very important. If we don't export the page, we can't use it LOL
 export default ArtistsPage;
