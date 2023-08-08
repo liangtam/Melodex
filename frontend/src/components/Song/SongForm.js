647eed09c897b3787c75f592c3e2781ebf8eef3a
UBC Student <flora817@students.cs.ubc.ca>
2023-08-08 12:48:13 -0700

    Update SongForm.js

--- frontend/src/components/Song/SongForm.js
+++ frontend/src/components/Song/SongForm.js
@@ -8,6 +8,7 @@ const SongForm = () => {
             <div className={styles.formContainer}>
             <h3>Add New Song</h3>
             <label>Song Name: <input type="text" placeholder="Name"></input></label>
+            <label>Artist Name: <input type="text" placeholder="Name(s)"></input></label>
             <label>Duration: <input type="text" placeholder="The length of song"></input></label>
             </div>
         </div>
