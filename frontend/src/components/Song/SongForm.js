7f95156d6f32ad9684337549dc3a3194575240b3
Ravenclawer <ljw0708@students.cs.ubc.ca>
2023-08-08 17:55:19 -0700

    delete num of likes for song

--- frontend/src/components/Song/SongForm.js
+++ frontend/src/components/Song/SongForm.js
@@ -2,23 +2,35 @@ import { useState, useEffect } from "react";
 import styles from "./SongForm.module.css";
 
 const SongForm = () => {
-
-    return (
-        <div>
-            <div className={styles.formContainer}>
-            <h3>Song</h3>
-            <label>Song Name: <input type="text" placeholder="Name"></input></label>
-            <label>Artist Name: <input type="text" placeholder="Name(s)"></input></label>
-            <label>Genre: <input type="text" placeholder="Genre"></input></label>
-            <label>Release Date: <input type="text" placeholder="eg. 2000-00-00(?)"></input></label>
-            <label>Number of Likes: <input type="text" placeholder="1 or more"></input></label>
-            <label>Duration: <input type="text" placeholder="The length of song"></input></label>
-            <label><div className={styles.btn}>Add</div></label>
-            <label><div className={styles.btn}>Delete</div></label>
-            <label><div className={styles.btn}>Update</div></label>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div className={styles.formContainer}>
+        <h3>Song</h3>
+        <label>
+          Song Name: <input type="text" placeholder="Name"></input>
+        </label>
+        <label>
+          Genre: <input type="text" placeholder="Genre"></input>
+        </label>
+        <label>
+          Release Date:{" "}
+          <input type="text" placeholder="eg. 2000-00-00(?)"></input>
+        </label>
+        <label>
+          Duration: <input type="text" placeholder="The length of song"></input>
+        </label>
+        <label>
+          <div className={styles.btn}>Add</div>
+        </label>
+        <label>
+          <div className={styles.btn}>Delete</div>
+        </label>
+        <label>
+          <div className={styles.btn}>Update</div>
+        </label>
+      </div>
+    </div>
+  );
+};
 
 export default SongForm;
