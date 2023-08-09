cbfc7251d3d1435392b734e85400db6fb43dea17
Ravenclawer <ljw0708@students.cs.ubc.ca>
2023-08-08 17:58:21 -0700

    delete artist name and numoflikes from album tuple

--- frontend/src/components/Album/AlbumTuple.js
+++ frontend/src/components/Album/AlbumTuple.js
@@ -2,39 +2,33 @@ import { useState, useEffect } from "react";
 import styles from "./AlbumTuple.module.css";
 
 const AlbumTuple = () => {
-
-    return (
-        <div>
-            <div className={styles.tupleContainer}>
-            <h3>Album List</h3>
-            <div className={styles.titlebox}>
-                <div className={styles.titlelist}>
-                    <div className={styles.titlecontent}>
-                    <h4>Album Name</h4>
-                    <h4>Artist Name</h4>
-                    <h4>Genre</h4>
-                    <h4>Release Date</h4>
-                    <h4>Number of Likes</h4>
-                    <h4>Duration</h4>
-                </div>
-                </div>
-                </div>
-            <div className={styles.box}>
-                <div className={styles.list}>
-                    <div className={styles.content}>
-                        <p>eg.albumname</p>
-                        <p>eg.artistname</p>
-                        <p>eg.genre</p>
-                        <p>eg.releasedate</p>
-                        <p>eg.numoflikes</p>
-                        <p>eg.duration</p>
-                    </div>
-                </div>
+  return (
+    <div>
+      <div className={styles.tupleContainer}>
+        <h3>Album List</h3>
+        <div className={styles.titlebox}>
+          <div className={styles.titlelist}>
+            <div className={styles.titlecontent}>
+              <h4>Album Name</h4>
+              <h4>Genre</h4>
+              <h4>Release Date</h4>
+              <h4>Duration</h4>
             </div>
-
-            </div>
+          </div>
         </div>
-    )
-}
+        <div className={styles.box}>
+          <div className={styles.list}>
+            <div className={styles.content}>
+              <p>eg.albumname</p>
+              <p>eg.genre</p>
+              <p>eg.releasedate</p>
+              <p>eg.duration</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default AlbumTuple;
