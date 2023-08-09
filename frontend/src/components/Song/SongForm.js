13307fa29ffec974c72a963dd00c4a14e518fe45
UBC Student <flora817@students.cs.ubc.ca>
2023-08-08 18:15:40 -0700

    Update SongForm.js

--- frontend/src/components/Song/SongForm.js
+++ frontend/src/components/Song/SongForm.js
@@ -1,36 +1,55 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import styles from "./SongForm.module.css";
 
-const SongForm = () => {
-  return (
-    <div>
-      <div className={styles.formContainer}>
-        <h3>Song</h3>
-        <label>
-          Song Name: <input type="text" placeholder="Name"></input>
-        </label>
-        <label>
-          Genre: <input type="text" placeholder="Genre"></input>
-        </label>
-        <label>
-          Release Date:{" "}
-          <input type="text" placeholder="eg. 2000-00-00(?)"></input>
-        </label>
-        <label>
-          Duration: <input type="text" placeholder="The length of song"></input>
-        </label>
-        <label>
-          <div className={styles.btn}>Add</div>
-        </label>
-        <label>
-          <div className={styles.btn}>Delete</div>
-        </label>
-        <label>
-          <div className={styles.btn}>Update</div>
-        </label>
-      </div>
-    </div>
-  );
-};
+
+const SongForm = (props) => {
+
+    const [song, setSong] = useState({
+        name: "",
+        artist: "",
+        genre: "",
+        releaseDate: "",
+        duration: ""
+    });
+
+    const [songs, setSongs] = useState([]);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setSong(prev => ({
+            ...prev,
+            [name]: value
+        }));
+    };
+
+    const handleAdd = () => {
+        props.addSong(song);
+        resetForm();
+    };
+
+    const resetForm = () => {
+        setSong({
+            name: "",
+            artist: "",
+            genre: "",
+            releaseDate: "",
+            duration: ""
+        });
+    };
+
+    return (
+        <div>
+            <div className={styles.formContainer}>
+                <label>Song Name: <input type="text" placeholder="Name" name="name" value={song.name} onChange={handleChange}></input></label>
+                <label>Artist Name: <input type="text" placeholder="Name(s)" name="artist" value={song.artist} onChange={handleChange}></input></label>
+                <label>Genre: <input type="text" placeholder="Genre" name="genre" value={song.genre} onChange={handleChange}></input></label>
+                <label>Release Date: <input type="text" placeholder="eg. 2000-00-00(?)" name="releaseDate" value={song.releaseDate} onChange={handleChange}></input></label>
+                <label>Duration: <input type="text" placeholder="The length of song" name="duration" value={song.duration} onChange={handleChange}></input></label>
+                <div className={styles.btn} onClick={handleAdd}>Add</div>
+            </div>
+        </div>
+    )
+}
 
 export default SongForm;
+
