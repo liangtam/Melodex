c907f0b8b6cba891802e6cf311f8f39d649d1fc1
Ravenclawer <ljw0708@students.cs.ubc.ca>
2023-08-08 23:22:24 -0700

    add state for albums page

--- frontend/src/pages/AlbumsPage.js
+++ frontend/src/pages/AlbumsPage.js
@@ -5,33 +5,31 @@ import AlbumForm from "../components/Album/AlbumForm";
 import AlbumTuple from "../components/Album/AlbumTuple";
 
 const AlbumsPage = () => {
+  const [albums, setAlbums] = useState([]);
 
-    const handleAddAlbum = (e) => {
-        e.preventDefault();
+  const addAlbum = (album) => {
+    setAlbums([...albums, album]);
+  };
 
-    }
-
-    return (
-        <div className={styles.albumContainer}>
-            <div className={styles.leftBody}>
-                <div className={styles.centered}>
-                    <div className={styles.title}>
-                        <AlbumTuple/>
-                    </div>
-                </div>
-            </div>
-            <div className={styles.rightBody}>
-                <div className={styles.centered}>
-                    <div className={styles.addForm}>
-                        <AlbumForm/>
-                    </div>
-                </div>
-            </div>
-            
+  return (
+    <div className={styles.albumContainer}>
+      <div className={styles.leftBody}>
+        <div className={styles.centered}>
+          <div className={styles.title}>
+            <AlbumTuple />
+          </div>
         </div>
-        
-    )
-}
+      </div>
+      <div className={styles.rightBody}>
+        <div className={styles.centered}>
+          <div className={styles.addForm}>
+            <AlbumForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 // DO NOT FORGET THIS LINE! It is very important. If we don't export the page, we can't use it LOL
 export default AlbumsPage;
